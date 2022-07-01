5784a9d1fb522ff51d89b7328dbc2286306b968b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-07-01 16:14:48 +0530

    Create an example file to test package

---
+++ example/index.js
@@ -0,0 +1,8 @@
+//  Library
+import { Status, StatusCode } from '../HTTPStatusCodes.js'
+
+console.log(Status.INFORMATION.CONTINUE)
+console.log(Status.INFORMATION[100])
+console.log(StatusCode.ACCEPTED)
+console.log(StatusCode[200])
+console.log(Status)
