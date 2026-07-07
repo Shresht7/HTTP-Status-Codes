67e522658941ec361c62be99b6722121a21d7fb6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-07-07 13:25:09 +0530

    Switch to `.ts` extension exports to prepare for deno

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { INFORMATION } from './information'
-import { SUCCESS } from './success'
-import { REDIRECT } from './redirect'
-import { CLIENT_ERROR } from './client_error'
-import { SERVER_ERROR } from './server_error'
+// Library
+import { INFORMATION } from './information.ts'
+import { SUCCESS } from './success.ts'
+import { REDIRECT } from './redirect.ts'
+import { CLIENT_ERROR } from './client_error.ts'
+import { SERVER_ERROR } from './server_error.ts'
 
 export { INFORMATION, SUCCESS, REDIRECT, CLIENT_ERROR, SERVER_ERROR }
 
