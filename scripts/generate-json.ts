6eae4aa84d4cb3a33cc03b847cd100487bb27dfc
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-07-07 14:29:00 +0530

    Migrate `generate-json.ts` to `generate-json.ps1` and utilize `deno doc --json` to extract descriptions straight from docstrings

--- scripts/generate-json.ts
+++
@@ -1,7 +0,0 @@
-import { Status, Code } from '../src/index.ts'
-
-const DIR = 'json'
-
-Deno.mkdirSync(DIR, { recursive: true })
-Deno.writeTextFileSync(`${DIR}/Status.json`, JSON.stringify(Status, null, 2))
-Deno.writeTextFileSync(`${DIR}/StatusCodes.json`, JSON.stringify(Code, null, 2))
